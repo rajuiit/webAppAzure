9a92c6d3b601667f923f4bcfdda2c00bc190cbd6
Md Mahfujur Rahman <mdmahfujurrahman@Mds-MacBook-Pro.local>
2024-11-18 02:43:14 +1000

    workshop

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+app.get('/workshop', (req, res) => {
+    res.send('We will attend');
+  });
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
